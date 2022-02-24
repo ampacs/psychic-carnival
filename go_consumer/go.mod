4d4e79e8d8d6310c6ba22fd9f8d8ddeb6c1afb02
André Santiago <andre.mpc.santiago@gmail.com>
2022-02-24 23:47:48 +0100

    feat: added initialization and router creation

--- go_consumer/go.mod
+++ go_consumer/go.mod
@@ -4,5 +4,7 @@ go 1.14
 
 require (
 	github.com/confluentinc/confluent-kafka-go v1.4.2 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/rs/cors v1.8.2
 	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.4.2
 )
--- go_consumer/main.go
+++ go_consumer/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+
+	"github.com/ampacs/psychic-carnival/go_consumer/api"
+	"github.com/ampacs/psychic-carnival/go_consumer/message"
 )
 
 func main() {
@@ -16,38 +23,51 @@ func main() {
 	}
 
 	// Connect consumer
-	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": broker,
 		"group.id":          "go_consumer",
 		"auto.offset.reset": "latest",
 	})
-
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("consumer creation error: %v", err))
 	}
+	defer func() {
+		if err = consumer.Close(); err != nil {
+			panic(fmt.Errorf("consumer closing error: %v", err))
+		}
+	}()
 
 	// Subscribe to the picture topic
-	c.SubscribeTopics([]string{"picture"}, nil)
-	ConsumeMessages(c)
-
-	c.Close()
-}
-
-// ConsumeMessages indefinitely consumes messages from the picture topic
-func ConsumeMessages(c *kafka.Consumer) {
-	for {
-		msg, err := c.ReadMessage(-1)
-		if err == nil {
-			HandleMessage(msg)
-		} else {
-			// The client will automatically try to recover from all errors.
-			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
-		}
+	if err = consumer.SubscribeTopics([]string{"picture"}, nil); err != nil {
+		panic(fmt.Errorf("topic subscription error: %v", err))
 	}
-}
 
-// HandleMessage handles a single message consumed from the picture topic
-func HandleMessage(message *kafka.Message) {
-	topic := *message.TopicPartition.Topic
-	fmt.Printf("[GoLang] Received a message on topic %s: %s\n", topic, string(message.Value))
+	// create a buffered channel to help prevent the message consumer from blocking
+	messageBuffer := make(chan message.Message, 10)
+	messageRepository := message.Repository{}
+
+	// create a message register that will update the repository
+	messageRegister := message.NewRegister(&messageRepository)
+	// messages are obtained from the channel
+	messageRegister.RegisterMessages(messageBuffer)
+
+	// create a message consumer
+	messageConsumer := message.NewConsumer(consumer)
+	// messages are sent through the buffer
+	messageConsumer.ConsumeMessages(messageBuffer)
+
+	// create the API with access to the repository
+	apiHandler := api.NewApi(&messageRepository)
+	router := mux.NewRouter().StrictSlash(true)
+	// register the routes
+	router.HandleFunc("/count", apiHandler.Count)
+	router.HandleFunc("/average-weight", apiHandler.AverageWeight)
+
+	// handle CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+	})
+
+	handler := c.Handler(router)
+	log.Fatal(http.ListenAndServe(":80", handler))
 }
